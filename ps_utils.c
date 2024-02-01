1541c632d3f20df75b82ff178a83e0af1e3ed565
iluvcasca <sntcillian@gmail.com>
2024-02-01 15:34:56 +0000

    First few step of parsing input
    For now prototype of parsing taking a string containing list of integer filling a circular doubly linked list with the integer.
    Quit on overflow.
    Quit on non-integer.
    
    TO DO:
     * Leaks
     * Take multiple argument
     * Norm"

--- ps_utils.c
+++ ps_utils.c
@@ -6,16 +6,49 @@
 /*   By: kgriset <kgriset@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/29 18:51:24 by kgriset           #+#    #+#             */
-/*   Updated: 2024/01/30 14:42:15 by kgriset          ###   ########.fr       */
+/*   Updated: 2024/02/01 15:29:57 by kgriset          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include "libft/mylibc/mylibc_local.h"
 #include "push_swap.h"
 
-int *parse_input(int argc, char ** argv)
+int parse_input(int argc, char ** argv, t_circular_double_link_list * cdl_list)
 {
-    int * numbers_lst;
+    int status;
+    char ** inputs;
+    int temp_int;
+    size_t i;
+    t_double_link_node * cdl_node;
 
-    if (argc > 2)
+    status = SUCCESS;
+    inputs = NULL;
+    temp_int = 0;
+    i = 0;
+    cdl_init_list(cdl_list);
+    if (argc == 2)
     {
+        inputs = ft_split(argv[1], ' ');
+        while(inputs[i])
+        {
+            temp_int = ft_atoi_safe(inputs[i], &status);
+            if (status)
+            {
+                cdl_node = malloc(sizeof(*cdl_node)); 
+                if (cdl_node && cdl_list->pf_insert_end(cdl_list, cdl_node))
+                {
+                    cdl_node->data = malloc(sizeof(temp_int));;
+                    *((int *)cdl_node->data) = temp_int;
+                }
+            }
+            else 
+                return (status);
+            i++;
+        }
     }
+    cdl_node = cdl_list->first_node;
+    do {
+        ft_printf("%d\n", *((int *)cdl_node->data));
+        cdl_node = cdl_node->next;
+    } while (cdl_node != cdl_list->first_node);
+    return (status);
 }
--- push_swap.c
+++ push_swap.c
@@ -1,19 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   push_swap.h                                        :+:      :+:    :+:   */
+/*   push_swap.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: kgriset <kgriset@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/30 11:50:43 by kgriset           #+#    #+#             */
-/*   Updated: 2024/02/01 14:59:30 by kgriset          ###   ########.fr       */
+/*   Created: 2024/02/01 14:58:30 by kgriset           #+#    #+#             */
+/*   Updated: 2024/02/01 15:32:14 by kgriset          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include "push_swap.h"
 
-#ifndef PUSH_SWAP
-# define PUSH_SWAP
-
-#include "libft/mylibc.h"
-
-int parse_input(int argc, char ** argv, t_circular_double_link_list * cdl_list);
-#endif
+int main()
+{
+    int argc = 2;
+    char * argv[2];
+    argv[0] = "a.out";
+    argv[1] = "2 1 3 6 5 8 ";
+    t_circular_double_link_list cdl_list;
+    int status = parse_input(argc, argv, &cdl_list); 
+    if (!status)
+        ft_printf("error\n");
+    else
+        ft_printf("success\n");
+    return (1);
+}
