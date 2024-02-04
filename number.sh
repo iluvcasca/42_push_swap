73e73449840949afbfd93d5d80b4b85d246af263
iluvcasca <sntcillian@gmail.com>
2024-02-04 17:49:08 +0000

    Add script for push_swap input
    the script take the number of value to be created and output random unique positive value

---
+++ number.sh
@@ -0,0 +1,20 @@
+# Define the number of unique random numbers to generate
+num=$1
+
+# Initialize an empty array
+random_numbers=()
+
+# While the length of the array is less than num
+while [[ ${#random_numbers[@]} -lt $num ]]; do
+    # Generate a random number
+    random_number=$(( RANDOM % 700 + 1 ))
+
+    # If the random number is not already in the array, add it
+    if ! [[ " ${random_numbers[@]} " =~ " ${random_number} " ]]; then
+        random_numbers+=(${random_number})
+    fi
+done
+
+# Print the array of unique random numbers
+echo "${random_numbers[@]}"
+
