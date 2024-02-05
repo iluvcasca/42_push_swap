2daca994897d19ed469f4a4a258eb6ddf7010b98
iluvcasca <sntcillian@gmail.com>
2024-02-05 20:28:32 +0000

    Add visualization for testing

--- push_swap.c
+++ push_swap.c
@@ -6,34 +6,79 @@
 /*   By: kgriset <kgriset@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/01 14:58:30 by kgriset           #+#    #+#             */
-/*   Updated: 2024/02/04 17:41:01 by kgriset          ###   ########.fr       */
+/*   Updated: 2024/02/05 19:58:51 by kgriset          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "push_swap.h"
 
 int main(int argc, char ** argv)
 {
-    t_circular_double_link_list cdl_list;
+    t_circular_double_link_list cdl_list_a;
+    t_circular_double_link_list cdl_list_b; // how much can the stack handle
+
     int status;
     
-    // argc = 2;
-    // argv[1] = "";
-    cdl_init_list(&cdl_list);
+    argc = 2;
+    argv[1] = "129 67 55 576 308 71 72 215 643 682 363 18 326 30 377";
+    char * s = "----------------------------\n";
+    //
+    cdl_init_list(&cdl_list_a);
+    cdl_init_list(&cdl_list_b);
     if (argc > 1)
-        status = parse_input(argc, argv, &cdl_list); 
+        status = parse_input(argc, argv, &cdl_list_a); 
     else
         return (ERROR);
     //
     if (status)
     {
-        ft_printf("\n");
-        int * array = cdl2array(&cdl_list);
-        array = bottom_up_merge_sort(array, cdl_list.total);
-        ft_printf("\n");
-        for(size_t i = 0; i < cdl_list.total; ++i)
-            ft_printf("%d\n", array[i]);
+        int * array = cdl2array(&cdl_list_a);
+        array = bottom_up_merge_sort(array, cdl_list_a.total);
+        ft_printf("%sInput\n%s", s, s);
+        print_cdl(&cdl_list_a);
+        // ft_printf("%sParse Sort\n%s", s, s);
+        // print_array(array, cdl_list_a.total);
+        ft_printf("%sOPS\n%s", s, s);
+
+        cdl_bottom_up_merge_sort(&cdl_list_a, &cdl_list_b);
+        // rotate(&cdl_list_a, 'a');
+        // push(&cdl_list_b, &cdl_list_a, 'b');
+        // r_rotate(&cdl_list_a, 'a');
+        print_cdl(&cdl_list_a);
+        print_cdl(&cdl_list_b);
+
         free (array);
-        cdl_free_list(&cdl_list);
+        cdl_free_list(&cdl_list_a);
+        cdl_free_list(&cdl_list_b);
     }
     return (SUCCESS);
+}
+
+void print_array(int * array, size_t size)
+{
+    size_t i;
+    
+    i = 0;
+    ft_printf("\n");
+    while (i < size)
+    {
+        ft_printf("%d\n", array[i]);
+        ++i;
+    }
+}
+
+void print_cdl(t_circular_double_link_list * cdl_list)
+{
+    t_double_link_node * cdl_node;
+
+    if (cdl_list && cdl_list->first_node)
+    {
+        ft_printf("\n");
+        cdl_node = cdl_list->first_node;
+        do {
+            ft_printf("%d\n", *((int *)cdl_node->data));
+            cdl_node = cdl_node->next;
+        } while (cdl_node != cdl_list->first_node);
+    }
+    if (cdl_list)
+        ft_printf("total: %u\n", cdl_list->total);
 }
